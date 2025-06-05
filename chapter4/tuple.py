379c081c9b218fc0ca4647e30130733ca5a64f90
Yash Yogesh Bora <yashbora567@gmail.com>
2025-06-05 09:35:41 +0530

    Add tuple.py

---
+++ chapter4/tuple.py
@@ -0,0 +1,11 @@
+#tuple is an immutable data type as list 
+a=(1,2,3,4,4,5)
+print(type(a))
+b=("harsh",'akash',"""hi""","abhi",1,323)
+print(b)
+no=a.count(4)
+print(no)
+i=a.index(4)
+print(i)
+c= a+b
+print(c)
